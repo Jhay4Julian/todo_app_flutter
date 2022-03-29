2b3cfef73f0654a516f82fd308056d8727a77d1c
JulianMozi <Jkoripamo@gmail.com>
2022-03-29 16:26:46 +0100

    set controllers & setstate

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,44 @@ void main() {
   runApp(const TodoApp());
 }
 
-class TodoApp extends StatelessWidget {
+class TodoApp extends StatefulWidget {
   const TodoApp({ Key? key }) : super(key: key);
 
+  @override
+  State<TodoApp> createState() => _TodoAppState();
+}
+
+class _TodoAppState extends State<TodoApp> {
+  // to save data
+  final List<String> _todoList = <String>[];
+  // for text field
+  final TextEditingController _textEditingController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('To-do list'),
+          title: const Text('To-do list'),
         ),),
     );
   }
+
+  // for adding data to list
+  void _addTodoItem(String title) {
+    // to notify the app that the state has changed
+    setState(() {
+      _todoList.add(title);
+    });
+    // to clear the textfield once an item is added to the list
+    _textEditingController.clear();
+  }
+
+  // to populate the listview
+  Widget _buildTodoItem(String title) {
+    return ListTile(title: Text(title),);
+  }
+
+
+
+
 }
