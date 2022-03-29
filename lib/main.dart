61bbf0194672890ec7eb09998bf5fee7af69f461
JulianMozi <Jkoripamo@gmail.com>
2022-03-29 17:52:52 +0100

    added dialog box & changed the app state

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const TodoApp());
+  runApp(TodoApp());
 }
 
-class TodoApp extends StatefulWidget {
-  const TodoApp({ Key? key }) : super(key: key);
-
-  @override
-  State<TodoApp> createState() => _TodoAppState();
-}
-
-class _TodoAppState extends State<TodoApp> {
-  // to save data
-  final List<String> _todoList = <String>[];
-  // for text field
-  final TextEditingController _textEditingController = TextEditingController();
+class TodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('To-do list'),
-        ),),
+      home: TodoList(),
     );
+  }
+}
+
+class TodoList extends StatefulWidget {
+  @override
+  State<TodoList> createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  // to save data
+  final List<String> _todoList = <String>[];
+  // for text field
+  final TextEditingController _textFieldController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Center(child: const Text('To-do list')),
+        ),
+        body: ListView(children: _getItems(),),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => _showMyDialog(context),
+          child: const Icon(Icons.add),
+          tooltip: 'Add Item',
+        ),
+      );
   }
 
   // for adding data to list
@@ -33,7 +45,7 @@ class _TodoAppState extends State<TodoApp> {
       _todoList.add(title);
     });
     // to clear the textfield once an item is added to the list
-    _textEditingController.clear();
+    _textFieldController.clear();
   }
 
   // to populate the listview
@@ -41,7 +53,43 @@ class _TodoAppState extends State<TodoApp> {
     return ListTile(title: Text(title),);
   }
 
+  // to display a dialog for the user to input items
+  Future<void> _showMyDialog(BuildContext context) async {
+    return showDialog(
+      context: context, 
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Add a task'),
+          content: TextField(
+            controller: _textFieldController,
+          ),
+          actions: [
+            TextButton(
+              onPressed: (){
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancel')
+              ),
+            TextButton(
+              onPressed: (){
+                Navigator.of(context).pop();
+                _addTodoItem(_textFieldController.text);
+              }, 
+              child: const Text('Add')
+              ),
+          ],
+        );
+      }
+      );
+  }
 
+  List<Widget> _getItems(){
+    final List<Widget> _todoWidgets = <Widget>[];
+    for (String title in _todoList) {
+      _todoWidgets.add(_buildTodoItem(title));
+    }
+    return _todoWidgets;
+  }
 
 
 }
